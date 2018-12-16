60c9def71ba2235b5bacaa0498ff826606ae6626
JDat <3792125+JDat@users.noreply.github.com>
2018-12-16 09:39:17 +0200

    Update sonicIPinstall.sh

--- sonicIPinstall.sh
+++ sonicIPinstall.sh
@@ -5,9 +5,7 @@ destination=$HOME/bin
 mkdir $destination
 mkdir $destination"/sonicIP"
 
-chmod 744 $destination"/sonicIP.sh"
 cp ./sonicIP.sh $destination/
 chmod 744 $destination"/sonicIP.sh"
 
 cp ./sonicIP/* $destination/sonicIP/
-
